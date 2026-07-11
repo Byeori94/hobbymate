71f3a3709e6f867c0289c32732b3e67c0dd5b71d
Byeori94 <sunwoo031094@gmail.com>
2026-07-11 13:18:26 +0900

    feat: HobbyMate 프로젝트 초기 구성

---
+++ src/main/java/com/byeori/hobbymate/HobbymateApplication.java
@@ -0,0 +1,13 @@
+package com.byeori.hobbymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HobbymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HobbymateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byeori/hobbymate/HobbymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byeori.hobbymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HobbymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
